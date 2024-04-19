d8d555f2ef6181ba268c778218c6884c9cd55d2c
Chaosborne <rick.chaosborne@gmail.com>
2024-04-19 14:49:12 +0300

    dishes components

---
+++ src/components/Meals/MealList.js
@@ -0,0 +1,40 @@
+import styles from './MealList.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Naomi Roll',
+    description: 'Philadelphia cheese, chicken fillet, masago, tomato, cucumber, sesame',
+    price: 11.99,
+  },
+  {
+    id: 'm2',
+    name: 'Spice in the salmon',
+    description: 'Rice, salmon, spice sauce',
+    price: 3.99,
+  },
+  {
+    id: 'm3',
+    name: 'Eel sushi',
+    description: 'Smoked eel, unagi sauce, sesame',
+    price: 4.99,
+  },
+  {
+    id: 'm4',
+    name: 'Poke Salad with Salmon',
+    description: 'Rice, salmon, cucumber, chuka, nori, tuna chips, walnut sauce',
+    price: 7.99,
+  },
+];
+
+const MealList = () => {
+  const mealList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+
+  return (
+    <section className={styles.meals}>
+      <ul>{mealList}</ul>
+    </section>
+  );
+};
+
+export default MealList;
---
+++ src/components/Meals/PromoText.js
@@ -0,0 +1,13 @@
+import styles from './PromoText.module.css';
+
+const PromoText = () => {
+  return (
+    <section className={styles['promo-text']}>
+      <h2>Online sushi restaurant Japanese Cuisine</h2>
+      <p>Japanese Cousine is an online sushi restaurant where your favorite sushi and sashimi, rolls and other dishes of national Japanese cuisine are made by a team of professional chefs</p>
+      <p>Fast work and quality products, as well as the most authentic ingredients give good flavor to the dishes, give pleasure from the meal</p>
+    </section>
+  );
+};
+
+export default PromoText;
