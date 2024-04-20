028344db871aa01831289fe200f8408e630ba1a2
Chaosborne <rick.chaosborne@gmail.com>
2024-04-20 17:34:17 +0300

    input hardcoded id fix

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -30,7 +30,7 @@ const DUMMY_MEALS = [
 ];
 
 const MealList = () => {
-  const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
+  const mealList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} />);
 
   return (
     <section className={styles.meals}>
