3acfd93eee56b26b48ca840d3972ff3c87a5d471
Chaosborne <rick.chaosborne@gmail.com>
2024-04-22 18:10:04 +0300

    cart btn and modal close state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartIsVisible, setCartIsVisible] = useState(false);
+
+  const showCarthandler = () => {
+    setCartIsVisible(true);
+  };
+
+  const hideCarthandler = () => {
+    setCartIsVisible(false);
+  };
+
   return (
     <React.Fragment>
-      <Cart />
-      <Header />
+      {cartIsVisible && <Cart onHideCart={hideCarthandler} />}
+      <Header onShowCart={showCarthandler} />
       <main>
         <Meals />
       </main>
